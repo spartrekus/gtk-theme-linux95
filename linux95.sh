33b231335cb19405183e552e4b1422b8e5afba35
spartrekus <spartrekus@gmail.com>
2017-10-29 15:06:07 +0100

    Update linux95.sh

--- linux95.sh
+++ linux95.sh
@@ -1,10 +1,13 @@
 #/bin/bash
 
   cd 
-  wget "https://codeload.github.com/spartrekus/gtk-theme-linux95/zip/master"            -O ~/linux95.zip 
+  mkdir tmp 
   cd 
+  cd tmp
+  
+  wget "https://codeload.github.com/spartrekus/gtk-theme-linux95/zip/master"            -O linux95.zip 
   unzip linux95.zip
-  cd ~/gtk-theme-linux95-master
+  cd gtk-theme-linux95-master
   unzip gtk-theme-linux95.zip 
   cp -v installer.sh linux95 
   cd linux95 
