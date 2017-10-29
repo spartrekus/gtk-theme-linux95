aadddc38a03911d89743bfa6cf71b79fc6523ad4
spartrekus <spartrekus@gmail.com>
2017-10-29 15:03:07 +0100

    Add files via upload

---
+++ linux95.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+  cd 
+  wget "https://codeload.github.com/spartrekus/gtk-theme-linux95/zip/master"            -O ~/linux95.zip 
+  cd 
+  unzip linux95.zip
+  cd ~/gtk-theme-linux95-master
+  unzip gtk-theme-linux95.zip 
+  cp -v installer.sh linux95 
+  cd linux95 
+  xterm 
+
+
